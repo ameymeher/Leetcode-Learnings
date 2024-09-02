6e2964bc48940e267b89075e6a3ca82eeaa23afb
“Amey” <meher.amey23@gmail.com>
2024-09-03 00:05:51 +0530

    Remove function in trie

--- Data Structures/Trie.py
+++ Data Structures/Trie.py
@@ -38,10 +38,70 @@ class Trie:
                 return False
         return True
     
+    # Remove a word from the trie
+    def remove(self,word):
+        def remove_word(word,curr_trie):
+            if word == '':
+                del curr_trie['*']
+                return not curr_trie
+            elif word[0] not in curr_trie:
+                return False
+            else:
+                if remove_word(word[1:],curr_trie[word[0]]) and not curr_trie[word[0]]:
+                    del curr_trie[word[0]]
+                    return True
+                return False
+        remove_word(word,self.trie)
+                    
+        
     def __repr__(self):
         return str(self.trie)
     
 # Example
-words = ['hello','world','he','hell']
+
+print("Creating a trie with words hello, world, he, hell, ''")
+words = ['hello','world','he','hell','']
 trie = Trie(words=words)
-print(trie)
+
+print("Trie: ",trie)
+print()
+
+print("Removing help from trie")
+trie.remove('help')
+print("Checking hello in trie: ",trie.search('hello'))
+print("Checking world in trie: ",trie.search('world'))
+print("Checking he in trie: ",trie.search('he'))
+print("Checking hell in trie: ",trie.search('hell'))
+print("Checking '' in trie: ",trie.search(''))
+print("Checking help in trie: ",trie.search('help'))
+print()
+
+print("Removing he from trie")
+trie.remove('he')
+print("Checking hello in trie: ",trie.search('hello'))
+print("Checking world in trie: ",trie.search('world'))
+print("Checking he in trie: ",trie.search('he'))
+print("Checking hell in trie: ",trie.search('hell'))
+print("Checking '' in trie: ",trie.search(''))
+print("Checking help in trie: ",trie.search('help'))
+print()
+
+print("Removing heyy from trie")
+trie.remove('heyy')
+print("Checking hello in trie: ",trie.search('hello'))
+print("Checking world in trie: ",trie.search('world'))
+print("Checking he in trie: ",trie.search('he'))
+print("Checking hell in trie: ",trie.search('hell'))
+print("Checking '' in trie: ",trie.search(''))
+print("Checking help in trie: ",trie.search('help'))
+print()
+
+print("Removing hello from trie")
+trie.remove('hello')
+print("Checking hello in trie: ",trie.search('hello'))
+print("Checking world in trie: ",trie.search('world'))
+print("Checking he in trie: ",trie.search('he'))
+print("Checking hell in trie: ",trie.search('hell'))
+print("Checking '' in trie: ",trie.search(''))
+print("Checking help in trie: ",trie.search('help'))
+print()
